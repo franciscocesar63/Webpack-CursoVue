8cc995a83aed5689c73a1a2df82bfe517c934ee1
franciscocesar63 <franciscocesar888@gmail.com>
2021-07-13 14:26:39 -0300

    Css-Loader e Style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-
 module.exports = {
 
     entry: './app/src/js/app.js',
     output: {
         filename: 'bundle.js',
-        path:  path.resolve('app/dist'),
+        path: path.resolve('app/dist'),
         clean: true
 
     },
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+
+        ]
+
+    },
+
     plugins: [
         new HtmlWebpackPlugin({
             template: "./app/src/app.html",
@@ -18,6 +25,7 @@ module.exports = {
             hash: true
 
         })
+
 
     ]
 
